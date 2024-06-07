8aea9d9d6c22ea1c5ee1e34b858f85ca2d8efce9
makrem <makrem/ltifi.77@gmail.com>
2024-06-08 00:03:06 +0100

    edit the html notes to contain some history and general informations

--- data/html_notes.js
+++ data/html_notes.js
@@ -1,25 +1,33 @@
 const html = {
-  "Introduction": [
+  Introduction: [
     "<h2 style='text-align: center;'>Introduction to HTML</h2>",
-    "<h4><li><span style='color:#ff34f0;'>H</span>yper<span style='color:#ff34f0;'>T</span>ext <span style='color:#ff34f0;'>M</span>arkup <span style='color:#ff34f0;'>L</span>anguage (HTML):</li></h4>",
-    "<h6><li>it is not a programming language it is a language Dedicated to building a site structure</li>",
-    "<li>is a set of codes to building the stucture of the site</li>",
-    "<li>for example we can write these codes using these code editor : </li>",
-    "<ol>VS Code</ol><ol>Notepad</ol><ol>Atome</ol>",
-    "<li>for example if you want to downlowd VS Code :</li>",
-    "<ol>First go to the browser </ol> <ol>then you will write vscode</ol> <ol>then chose the first link</ol> <ol>then you will find a button to start downlowd</ol> <ol>Finely you can start coding</ol> ",
-    "<li> If we say sites we say web then We will create websites the web domain is divided into two specializations : </li>",
-    "<ol>Front end development</ol> <ol>Back end development</ol> </h6>"
+    "<h4><li><span style='color:#ff34f0;'>H</span>yper<span style='color:#ff34f0;'>T</span>ext <span style='color:#ff34f0;'>M</span>arkup <span style='color:#ff34f0;'>L</span>anguage (HTML)</li></h4>",
+    "<h6><li>HTML is the standard markup language for creating web pages and web applications.</li>",
+    "<li>It was created by Tim Berners-Lee in late 1991 but was not officially published until 1993.</li>",
+    "<li>HTML is used to describe the structure of web pages using markup. Elements are the building blocks of HTML pages.</li>",
+    "<li>HTML elements are represented by tags such as &lt;head&gt;, &lt;title&gt;, &lt;body&gt;, &lt;header&gt;, &lt;footer&gt;, &lt;article&gt;, &lt;section&gt;, and many others.</li>",
+    "<li>Over the years, HTML has undergone several updates and versions. The latest version is HTML5, which includes new elements, attributes, and behaviors.</li>",
+    "<li>HTML5 also supports multimedia elements like &lt;audio&gt; and &lt;video&gt;, as well as new APIs to enhance web functionality.</li>",
+    "<li>To write HTML, you can use code editors such as:</li>",
+    "<ol><li>VS Code</li><li>Notepad</li><li>Atom</li></ol>",
+    "<li>To download VS Code:</li>",
+    "<ol><li>Open your browser.</li><li>Search for 'VS Code'.</li><li>Click on the first link.</li><li>Click the download button.</li><li>Install and start coding.</li></ol>",
+    "<li>When we talk about websites, we often refer to two main specializations:</li>",
+    "<ol><li>Front-end development</li><li>Back-end development</li></ol>",
   ],
 
-  "site composition": ["<h2 style='text-align: center;'>HTML Tags</h2>",
-  "<h6><li>if you want to builde a website you have to understand what websites are made of usually websites made of group of files for example :</li>",
-  "<ol>HTML files</ol> ",
-  "<ol>cSS files</ol> ",
-  "<ol>JavaScript files</ol> ",
-  "</h6>" 
-
+  "site composition": [
+    "<h2 style='text-align: center;'>HTML Tags</h2>",
+    "<h6><li>if you want to builde a website you have to understand what websites are made of usually websites made of group of files for example :</li>",
+    "<ol>HTML files</ol> ",
+    "<ol>cSS files</ol> ",
+    "<ol>JavaScript files</ol> ",
+    "</h6>",
   ],
-  "Attributes": ["<h2 style='text-align: center;'>HTML Attributes</h2><p>This is the section about HTML attributes.</p>"],
-  "Forms": ["<h2 style='text-align: center;'>HTML Forms</h2><p>This is the section about HTML forms.</p>"]
+  Attributes: [
+    "<h2 style='text-align: center;'>HTML Attributes</h2><p>This is the section about HTML attributes.</p>",
+  ],
+  Forms: [
+    "<h2 style='text-align: center;'>HTML Forms</h2><p>This is the section about HTML forms.</p>",
+  ],
 };
