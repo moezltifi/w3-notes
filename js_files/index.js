19d1fe73b3ccc4b2f937be67c6804b3e23835959
makrem <makrem/ltifi.77@gmail.com>
2024-06-08 09:39:56 +0100

    add two buttons prev and next in the view

--- js_files/index.js
+++ js_files/index.js
@@ -4,60 +4,60 @@ const Javascript = Object.keys(javascript);
 const sections = {
   HTML: Html,
   CSS: Css,
-  JavaScript: Javascript
+  JavaScript: Javascript,
 };
 function showSections(course) {
-  const sidebar = document.getElementById('mySidebar');
-  const content = document.getElementById('content');
-  sidebar.innerHTML = ''; 
-  content.innerHTML = '';
-  const buttons = document.querySelectorAll('.w3-button');
-  buttons.forEach(function(button) {
-    button.style.backgroundColor = '';
+  const sidebar = document.getElementById("mySidebar");
+  const content = document.getElementById("content");
+  sidebar.innerHTML = "";
+  content.innerHTML = "";
+  const buttons = document.querySelectorAll(".w3-button");
+  buttons.forEach(function (button) {
+    button.style.backgroundColor = "";
   });
   const clickedButton = document.getElementById(course);
-  clickedButton.style.backgroundColor = '#04aa6d';
-  sections[course].forEach(function(Content) {
-    const link = document.createElement('button');
-    link.classList.add('w3-bar-item', 'w3-button');
+  clickedButton.style.backgroundColor = "#04aa6d";
+  sections[course].forEach(function (Content) {
+    const link = document.createElement("button");
+    link.classList.add("w3-bar-item", "w3-button");
     link.textContent = Content;
-    link.onclick = function() {
-      const clickedSection = sidebar.querySelector('.selected');
+    link.onclick = function () {
+      const clickedSection = sidebar.querySelector(".selected");
       if (clickedSection) {
-        clickedSection.classList.remove('selected');
+        clickedSection.classList.remove("selected");
       }
-      link.classList.add('selected');
+      link.classList.add("selected");
       showContent(course, Content);
     };
     sidebar.appendChild(link);
   });
-  const introductionButton = sidebar.querySelector('button');
+  const introductionButton = sidebar.querySelector("button");
   if (introductionButton) {
-    introductionButton.classList.add('selected');
+    introductionButton.classList.add("selected");
     showContent(course, sections[course][0]);
   }
 }
 function showContent(course, section) {
-  let contentToDisplay = '';
+  let contentToDisplay = "";
   if (section === Object.keys(course.toLowerCase())[0] || section === "") {
     contentToDisplay = html.Introduction.join("");
-  }else{
-  switch (course) {
-    case 'HTML':
+  } else {
+    switch (course) {
+      case "HTML":
         contentToDisplay = html[section].join("");
-      break;
-    case 'CSS':
+        break;
+      case "CSS":
         contentToDisplay = css[section].join("");
-      break;
-    case 'JavaScript':
+        break;
+      case "JavaScript":
         contentToDisplay = javascript[section].join("");
-      break;
-    default:
-      contentToDisplay = "<h2>No content available</h2>";
-      break;
+        break;
+      default:
+        contentToDisplay = "<h2>No content available</h2>";
+        break;
+    }
+    const content = document.getElementById("content");
+    content.innerHTML = contentToDisplay;
   }
-  const content = document.getElementById('content');
-  content.innerHTML = contentToDisplay;
 }
-}
-showSections('HTML');
+showSections("HTML");
